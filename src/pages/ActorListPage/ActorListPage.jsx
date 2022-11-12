07c76da165f0108a390684c9eff462fc7049b730
Colton Rennie <colton.rennie@hotmail.com>
2022-11-12 13:56:37 -0400

    App and Skeleton all set up

---
+++ src/pages/ActorListPage/ActorListPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ActorListPage() {
+  return (
+    <div>
+      <h1>ActorListPage</h1>
+    </div>
+  )
+}
---
+++ src/pages/MovieListPage/MovieListPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MovieListPage() {
+  return (
+    <div>
+      <h1>MovieListPage</h1>
+    </div>
+  )
+}
